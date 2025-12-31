2691eda9661abf71d1a15c3561391a710e34eb8f
SheHab970 <shehab202205097@gmail.com>
2025-12-31 23:05:39 +0200

    Responsive visual page for mobile sceen

--- visual/JS/visualscript.js
+++ visual/JS/visualscript.js
@@ -1,16 +1,36 @@
-      const img = document.getElementById("resultImage");
-      const text = document.getElementById("wordInput");
-      async function sendWord()
-      {
+const img = document.getElementById("resultImage");
+const text = document.getElementById("wordInput");
 
-      if (!text.value) return;
-        const response = await fetch(
-          `https://picto.runasp.net/api/Words/${encodeURIComponent(text.value)}`
-        );
+function showLoading() {
+  document.getElementById('loading-spinner').style.display = 'block';
+}
 
-        const data = await response.json();
-        document.getElementById("difficulty").innerText =
-          "Difficulty: " + data.difficultyLevel;
-        img.src = data.imageUrl;
-        img.style.display = "block";
-    }
+// Function to hide the loading indicator
+function hideLoading() {
+  document.getElementById('loading-spinner').style.display = 'none';
+}
+
+async function sendWord() {
+
+  if (!text.value) return;
+
+  showLoading(); // Set loading to true before the API call
+
+  try{
+    const response = await fetch(
+      `https://picto.runasp.net/api/Words/${encodeURIComponent(text.value)}`
+    );
+  
+    const data = await response.json();
+    document.getElementById("difficulty").innerText =
+      "Difficulty: " + data.difficultyLevel;
+    img.src = data.imageUrl;
+    img.style.display = "block";
+
+  } catch (error) {
+      console.error("Failed to load words", error);
+      
+  }finally{
+    hideLoading(); // Set loading to false after the API call is complete
+  }
+}
